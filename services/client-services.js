7d9422803f92be1776ead94b73b4a9468316a137
Nahuel Otamendi <nahuotamendi@gmail.com>
2023-05-15 07:38:10 -0300

    Servidor dado de alta

---
+++ services/client-services.js
@@ -0,0 +1,42 @@
+const crearNuevaLinea = () => {
+    `<tr>
+    <td class="td" data-td>Gabriela</td>
+    <td>gabriela@alura.com</td>
+    <td>
+      <ul class="table__button-control">
+        <li>
+          <a
+            href="../screens/editar_cliente.html"
+            class="simple-button simple-button--edit"
+            >Editar</a
+          >
+        </li>
+        <li>
+          <button
+            class="simple-button simple-button--delete"
+            type="button"
+          >
+            Eliminar
+          </button>
+        </li>
+      </ul>
+    </td>
+  </tr>`
+}
+
+const http = new XMLHttpRequest();
+//Abrit http (metodos, url)
+//CRUD   - Metodos HTTP
+//Create - POST
+//Read   - GET
+//Update - PUT/PATCH
+//Delete - DELETE
+
+http.open("GET", "http://localhost:3000/perfil")
+
+http.send();
+
+http.onload = () => {
+    const data = http.response;
+    console.log(data);
+}
