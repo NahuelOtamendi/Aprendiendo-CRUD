07a2676e6c765c30fe65bc122c1ac031e356a85f
Nahuel Otamendi <nahuotamendi@gmail.com>
2023-05-15 08:26:54 -0300

    Se creo una Promise basicamente para dar de alta un nuevo cliente

--- services/client-services.js
+++ services/client-services.js
@@ -1,42 +1,67 @@
-const crearNuevaLinea = () => {
-    `<tr>
-    <td class="td" data-td>Gabriela</td>
-    <td>gabriela@alura.com</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-            >Editar</a
-          >
-        </li>
-        <li>
-          <button
-            class="simple-button simple-button--delete"
-            type="button"
-          >
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  </tr>`
-}
+const crearNuevaLinea = (nombre, email) => {
+  const linea = document.createElement("tr");
 
-const http = new XMLHttpRequest();
+  const contenido = `
+        <td class="td" data-td>${nombre}</td>
+        <td>${email}</td>
+        <td>
+        <ul class="table__button-control">
+            <li>
+            <a
+                href="../screens/editar_cliente.html"
+                class="simple-button simple-button--edit"
+                >Editar</a
+            >
+            </li>
+            <li>
+            <button
+                class="simple-button simple-button--delete"
+                type="button"
+            >
+                Eliminar
+            </button>
+            </li>
+        </ul>
+        </td>`;
+  linea.innerHTML = contenido;
+  return linea;
+};
+
+const table = document.querySelector("[data-table]");
 //Abrit http (metodos, url)
 //CRUD   - Metodos HTTP
 //Create - POST
 //Read   - GET
 //Update - PUT/PATCH
 //Delete - DELETE
+const listaClientes = () => {
+  const promise = new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+    http.open("GET", "http://localhost:3000/perfil");
+    http.send();
 
-http.open("GET", "http://localhost:3000/perfil")
+    http.onload = () => {
+      const response = JSON.parse(http.response);
+      if (http.status >= 400) {
+        reject(response);
+      } else {
+        resolve(response);
+      }
+    };
+  });
+  return promise;
+};
 
-http.send();
+listaClientes().then((data) => {
+  data.forEach((perfil) => {
+    const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+    table.appendChild(nuevaLinea);
+  });
+}).catch(() => {
+    alert("Ocurrio un error")
+});
 
-http.onload = () => {
-    const data = http.response;
-    console.log(data);
-}
+// data.forEach((perfil) => {
+//   const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+//   table.appendChild(nuevaLinea);
+// });
